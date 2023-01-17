f06914d4dfdd6b3206e861f8ad8ced1ac5cbb00c
adhilameenet <adhilameenet@gmail.com>
2023-01-17 14:27:03 +0530

    add:init

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req,res) => {
+    res.send("Root")
+})
+app.get('/home', (req,res) => {
+    res.send("Home")
+})
+app.get('/about', (req,res) => {
+    res.send("About")
+})
+
+app.listen(3000 , () => {
+    console.log("Server started")
+})
